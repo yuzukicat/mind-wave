08c26f922a694e69a90415fab05ccc53f2bc8813
yuzukicat <yuzuki.cat@kamisu66.com>
2023-03-22 22:40:14 +0900

    fork && patch mind-wave to fit nixos

--- mind-wave.el
+++ mind-wave.el
@@ -194,7 +194,7 @@
   "Name of Mind-Wave buffer."
   :type 'string)
 
-(defcustom lsp-bridge-python-command "${python3.interpreter}"
+(defcustom lsp-bridge-python-command "${pkgs.python3.interpreter}"
   "The Python interpreter used to run mind_wave.py."
   :type 'string)
 
